a819dcfe26fef6928bf099cf1871cff2d32aab27
Codeforme <deveshtiwari2511@gmail.com>
2023-12-27 17:57:48 +0530

    deployment changes

--- server.py
+++ server.py
@@ -120,4 +120,4 @@ def upload_image():
         return jsonify({'error': 'Invalid file format'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host= "0.0.0.0")
